13f4c0ab23355a2e208cfdb53e8ebe16be3699ce
dospro <dospro@gmail.com>
2023-12-03 23:53:47 -0600

    Completed challenge 1

---
+++ challenge1/main.kts
@@ -0,0 +1,23 @@
+import java.nio.file.Paths
+
+/*
+Examples
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+
+Result: 142
+ */
+
+fun main() {
+    val text = Paths.get("input.txt").toFile().readText(Charsets.UTF_8)
+    val result = text.split("\n")
+        .filter { line -> line.isNotEmpty() }
+        .sumOf { line ->
+            line.first { char -> char.isDigit() }.digitToInt() * 10 + line.last { char -> char.isDigit() }.digitToInt()
+        }
+    println(result)
+}
+
+main()
